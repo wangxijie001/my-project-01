683eaa6b7a3acd4b3010b32354d9eb5f1b125af7
wangtong <wangtong@chukong-inc.com>
2025-09-28 14:21:41 +0800

    处理文件目录

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
+/*
+ * @Author: wangtong wangtong@chukong-inc.com
+ * @Date: 2025-09-24 17:39:47
+ * @LastEditors: wangtong wangtong@chukong-inc.com
+ * @LastEditTime: 2025-09-28 14:21:19
+ * @FilePath: /my-next-app/next.config.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  distDir: 'dist',  // 将默认的 .next 改为 dist
+  distDir: '.dist',  // 将默认的 .next 改为 .dist
 };
 
 export default nextConfig;
